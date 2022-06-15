1d9455ee591a5870639a1ed4dc4fcc9a7104bd88
trayneli <jtraynelis@gmail.com>
2022-06-15 19:07:43 +0000

    [pmc] update

--- R/process.R
+++ R/process.R
@@ -1,4 +1,3 @@
-library(R.utils)
 library(purrr)
 library(utils)
 library(tools)
@@ -8,24 +7,18 @@ library(tibble)
 library(data.table)
 library(dplyr)
 library(readr)
-library(httr)
 library(here)
-library(httr)
 library(stringr)
-library(tidyverse)
-library(dplyr)
 library(fs)
-library(purrr)
 library(arsenal)    
 library(tidyr)
 
-
 ##----------------- User Input location to download intermediate files
 if (interactive()) {
-  print("Format Example:Folder/to/File/")
+	  print("Format Example:Folder/to/File/")
   externalDriveLocation <- readline("Temporary File Storage Location:")
 } else {
-  cat("Temporary File Storage Location:")
+	  cat("Temporary File Storage Location:")
   externalDriveLocation <- readLines("stdin", n = 1)
 }
 
@@ -39,11 +32,11 @@ download_body <-file.path(externalDriveLocation,"output_pmc_txt/pmc_body")
 download_front <-file.path(externalDriveLocation,"output_pmc_txt/pmc_front")
 
 base_name <- function(filename) {
-  file_path_sans_ext(basename(filename))
+	  file_path_sans_ext(basename(filename))
 }
 mkdir = function (dir) {
-  if (!dir.exists(dir)) {
-    dir.create(dir,recursive=TRUE)
+	  if (!dir.exists(dir)) {
+		      dir.create(dir,recursive=TRUE)
   } 
 }
 map(c(data_dir,download_doc,download_txt,download_body,download_front),mkdir)
@@ -51,17 +44,17 @@ map(c(data_dir,download_doc,download_txt,download_body,download_front),mkdir)
 
 ##----------------- Process csv documentation folders
 process_documentation_files<-function(filename){
-  list.files(download_doc, full.names = TRUE, pattern = 'csv')|>
+	  list.files(download_doc, full.names = TRUE, pattern = 'csv')|>
     map(function(filename) {
-      df <- vroom::vroom(filename,',')
-      arrow::write_parquet(df,file.path(data_dir,paste0(base_name(filename),".parquet")))
-    })
+		      df <- vroom::vroom(filename,',')
+		            arrow::write_parquet(df,file.path(data_dir,paste0(base_name(filename),".parquet")))
+		          })
 }
 combineParquetDf<-function(parquet_tmp_list){
-  tmpDf<-data.table::rbindlist(lapply(Sys.glob(parquet_tmp_list), arrow::read_parquet), fill = TRUE)
+	  tmpDf<-data.table::rbindlist(lapply(Sys.glob(parquet_tmp_list), arrow::read_parquet), fill = TRUE)
   arrow::write_parquet(tmpDf, file.path(data_dir,paste0("pmc_data_documentation.parquet")))
-  rm(tmpDf)
-  map(parquet_tmp_list,file.remove)
+    rm(tmpDf)
+    map(parquet_tmp_list,file.remove)
 }
 
 process_documentation_files()
@@ -69,35 +62,40 @@ tmp_files=list.files(data_dir, pattern = '^oa', full.names = TRUE)
 combineParquetDf(tmp_files)
 
 ##----------------- Process pmc text data files
-split_list<-function(file_list){
-  listCount=length(file_list)/50000
-  splitFileJoinList=split(file_list, rep(1:listCount))
-  return(splitFileJoinList)
+split_list<-function(input_file){
+	  file_list=read.csv(input_file, header = FALSE)
+  listCount=length(file_list$V1)/5000
+    splitFileJoinList=split(file_list, rep(1:listCount))
+    return(splitFileJoinList)
 }
+
 parse_txt_data<-function(input_file){
-  parsed_file=read_file(input_file)
+	  parsed_file=read_file(input_file)
   output_tibble=tibble(parsed_file) |> mutate(fileid=base_name(input_file)) |> 
-    mutate(parsed_front_data=reduce2(c("\n==== Front\n","\n==== Body\n"), c('', ''),  .init = parsed_file, str_replace)) |>
-    select(parsed_front_data, fileid)
-  return(output_tibble)
+      mutate(parsed_front_data=reduce2(c("==== Front","==== Body"), c('',''),  .init = parsed_file, str_replace)) |>
+      select(parsed_front_data, fileid)
+        return(output_tibble)
 }
+
 combine_txt_data<-function(input_file_list, outputFileName){
-  output_data=list()
-  for( i in seq(1,length(input_file_list))){
-    output_data[[i]]=parse_txt_data(input_file_list[i])
-  }
-  combined_tibble = do.call(rbind, output_data)
-  arrow::write_parquet(combined_tibble, outputFileName)
-  rm(combined_tibble)
+	  output_data=list()
+  for( i in seq(1,length(input_file_list$V1))){
+	      output_data[[i]]=parse_txt_data(input_file_list$V1[i])
+    }
+    combined_tibble = do.call(rbind, output_data)
+    arrow::write_parquet(combined_tibble, outputFileName)
+      rm(combined_tibble)
 }
 combine_list_data<-function(all_data_lists, type_data){
-  for(i in seq(1,length(all_data_lists))){
-    combine_txt_data(all_data_lists[[i]], file.path(data_dir, paste0("PMC_data_shard_",type_data,"_",i,".parquet")))
+	  for(i in seq(1,length(all_data_lists))){
+		      combine_txt_data(all_data_lists[[i]], file.path(data_dir, paste0("PMC_data_shard_",type_data,"_",i,".parquet")))
   }  
 }
 
-list_split_body=split_list(file.path(list.files(download_body, pattern = 'txt', full.names = TRUE)))
-list_split_front=split_list(file.path(list.files(download_front, pattern = 'txt', full.names = TRUE)))
+list_split_body=split_list(file.path(download_dir,'tmp_pmc_body'))
+list_split_front=split_list(file.path(download_dir,'tmp_pmc_front'))
+
 combine_list_data(list_split_body, "body")
 combine_list_data(list_split_front, "front")
+
 
--- bash/parse_pmc.sh
+++ bash/parse_pmc.sh
@@ -2,30 +2,29 @@
 
 ##Set-up env on linux/ubuntu
 if aws --version; then
-    echo "Command succeeded: awscli installed"
-else
-  sudo apt install unzip;
-  wget "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -O "awscliv2.zip";
-  unzip -o awscliv2.zip;
-  sudo ./aws/install;
-  sudo apt-get update;
-  sudo apt-get install awscli;
+	    echo "Command succeeded: awscli installed"
+    else
+	      sudo apt install unzip;
+	        wget "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -O "awscliv2.zip";
+		  unzip -o awscliv2.zip;
+		    sudo ./aws/install;
+		      sudo apt-get update;
+		        sudo apt-get install awscli;
 fi
 
 if R --version; then
-  echo "Command succeeded: R installed"
-else
-  sudo apt install r-base-core
+	  echo "Command succeeded: R installed"
+  else
+	    sudo apt install r-base-core
 fi
 
 if dvc --version; then
-  echo "Command succeeded: dvc installed"
-else
-  sudo apt update;
-  sudo apt install snapd;
-  sudo snap install dvc --classic;
+	  echo "Command succeeded: dvc installed"
+  else
+	    sudo apt update;
+	      sudo apt install snapd;
+	        sudo snap install dvc --classic;
 fi
-
 
 ## Download documentation info for pmc data files:
 mkdir -p download/output_documentation;
@@ -38,28 +37,40 @@ DIR='output_pmc_txt';
 output_path=$user_path$DIR;
 mkdir -p $output_path
 if [ "$(ls -A $output_path)" ]; then
-   echo "$output_path is not Empty"
-else
-  aws s3 sync s3://pmc-oa-opendata/oa_comm/txt/all/ $output_path --no-sign-request;
+	   echo "$output_path is not Empty"
+   else
+	     aws s3 sync s3://pmc-oa-opendata/oa_comm/txt/all/ $output_path --no-sign-request;
 fi
-output_path=/data/output_pmc_txt
-## Split each text file into paper sections front and body: Reduces file size
-mkdir -p $output_path/pmc_front $output_path/pmc_body;
-for inputFile in $output_path/*.txt; do
-  csplit -ks -f $output_path/`basename $inputFile`_split $inputFile /==/ {2};
-  rm $inputFile;
-done
 
-## Move sections of pmc data to proper directory
-for i in $output_path/*split00*; do
-  rm $i
-done
-for i in $output_path/*split03*; do
-  rm $i
-done
-for i in $output_path/*split01*; do
-  mv $i $output_path/pmc_front;
-done
-for i in $output_path/*split02*; do
-  mv $i $output_path/pmc_body;
-done
+mkdir -p $output_path/pmc_front $output_path/pmc_body;
+if [[ "$(ls -A $output_path/pmc_front)" && "$(ls -A $output_path/pmc_body)" ]]; then
+	  echo "$output_path/pmc_body and $output_path/pmc_front are not Empty"
+  else
+	    ## Split each text file into paper sections front and body: Reduces file size
+	      for inputFile in $output_path/*.txt; do
+		          csplit -ks -f $output_path/`basename $inputFile`_split $inputFile /==/ {2};
+			      rm $inputFile;
+			        done
+				  ## Move sections of pmc data to proper directory
+				    for i in $output_path/*split00*; do
+					        rm $i
+						  done
+						    for i in $output_path/*split03*; do
+							        rm $i
+								  done
+								    for i in $output_path/*split01*; do
+									        mv $i $output_path/pmc_front;
+										  done
+										    for i in $output_path/*split02*; do
+											        mv $i $output_path/pmc_body;
+												  done
+fi
+
+## Generate list file:
+for i in $output_path/pmc_body/*; do
+	  echo $i;
+  done > download/tmp_pmc_body
+  for i in $output_path/pmc_front/*; do
+	    echo $i;
+    done > download/tmp_pmc_front
+
